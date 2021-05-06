3eec7a755856fe060ec023bbe2a1832c86955939
TamNguyen <ngductam91@gmail.com>
2021-05-06 23:22:26 +0700

    Product Discount Calculator

--- ProductDiscountCalculator/display_discount.php
+++ ProductDiscountCalculator/display_discount.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'index.php';
+    include_once 'index.php';
     if($_SERVER["REQUEST_METHOD"]== "POST"){
         $product = $_POST["product"];
         $price = $_POST["price"];
@@ -7,9 +7,10 @@ include_once 'index.php';
         $percent = 100;
         $discountAmount = $price * ($discountPercent/$percent);
         $discountPrice = $price - $discountAmount;
-        echo "Product Description is : "."$product"."<br>";
+        echo "<hr>"."Product Description is : "."$product"."<br>";
         echo "Price is : "."$price"."<br>";
         echo "Discount Percent is : "."$discountPercent"." % "."<br>";
         echo "Discount Amount is : "."$discountAmount"."<br>";
         echo "Discount Price is : "."$discountPrice";
     }
+

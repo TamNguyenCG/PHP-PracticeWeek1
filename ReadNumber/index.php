796a01fa40d788ebceb5a4bec46415f86e8b19cf
TamNguyen <ngductam91@gmail.com>
2021-05-06 21:53:36 +0700

    Read Numbers to Letters

--- ReadNumber/index.php
+++ ReadNumber/index.php
@@ -18,113 +18,166 @@
     </div>
 </form>
 <?php
-    if($_SERVER["REQUEST_METHOD"]=="GET") {
-        $number = $_GET["input"];
-        $unitsNumber = $number % 10;
-        $tensNumber = $number/10;
-        $readTensNumber = $tensNumber.$unitsNumber;
-//        echo "<script>console.log('$readTensNumber');</script>";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $so = $_GET["input"];
 
-        if ($number >= 0 && $number <= 10) {
-            switch ($number) {
-                case 0:
-                    echo "zero";
-                    break;
-                case 1:
-                    echo "one";
-                    break;
-                case 2:
-                    echo "two";
-                    break;
-                case 3:
-                    echo "three";
-                    break;
-                case 4:
-                    echo "four";
-                    break;
-                case 5:
-                    echo "five";
-                    break;
-                case 6:
-                    echo "six";
-                    break;
-                case 7:
-                    echo "seven";
-                    break;
-                case 8:
-                    echo "eight";
-                    break;
-                case 9:
-                    echo "nine";
-                    break;
-                case 10:
-                    echo "ten";
-                    break;
+
+    $tram = floor($so / 100);
+    $chuc = floor(($so - ($tram * 100)) / 10);
+    $don_vi = $so - $tram * 100 - $chuc * 10;
+
+
+    $mangDon_vi = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    $mangChuc = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+    $mang10_19 = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+
+    if ($so >= 0 && $so < 1000) {
+        // trường hợp số nhập vào là số có 1 chữ số
+        if ($tram == 0 && $chuc == 0 && $don_vi < 10) {
+            for ($i = 0; $i < count($mangDon_vi); $i++) {
+                switch ($don_vi) {
+                    case $i:
+                        echo "$so" . "=>" . "$mangDon_vi[$i]";
+                        break;
+                }
             }
         }
-        if ($number > 10 && $number < 20) {
-            switch ($unitsNumber) {
-                case 1:
-                    echo "eleven";
-                    break;
-                case 2:
-                    echo "twelve";
-                    break;
-                case 3:
-                    echo "thirteen";
-                    break;
-                case 4:
-                    echo "fourteen";
-                    break;
-                case 5:
-                    echo "fifteen";
-                    break;
-                case 6:
-                    echo "sixteen";
-                    break;
-                case 7:
-                    echo "seventeen";
-                    break;
-                case 8:
-                    echo "eighteen";
-                    break;
-                case 9:
-                    echo "nineteen";
-                    break;
+        // trường hợp số nhập vào là số có 2 chữ số
+        if ($tram == 0 && $chuc >= 1 && $chuc < 10 && $don_vi < 10) {
+            // trường hợp từ 10-19
+            if ($chuc == 1) {
+                for ($i = 0; $i < count($mang10_19); $i++) {
+                    switch ($don_vi) {
+                        case $i:
+                            echo "$so" . "=>" . "$mang10_19[$i]";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                // trường hợp từ 20 - 100
+            } else {
+                // trườg hợp các số chẵn chục từ 20 - 100
+                if ($don_vi == 0) {
+                    for ($i = 0; $i < count($mangChuc); $i++) {
+                        switch ($chuc) {
+                            case $i:
+                                echo "$so" . "=>" . "$mangChuc[$i]";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    // các TH còn lại từ 20 - 100
+                } else {
+                    for ($i = 0; $i < count($mangChuc); $i++) {
+                        for ($y = 0; $y < count($mangDon_vi); $y++) {
+                            switch ($chuc) {
+                                case $i:
+                                    switch ($don_vi) {
+                                        case $y:
+                                            echo "$so" . "=>" . "$mangChuc[$i]" . '-' . "$mangDon_vi[$y]";
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
             }
         }
-        if ($number >= 20 && $number <= 100) {
-            switch ($tensNumber) {
-                case 2:
-                    echo "twenty";
-                    break;
-                case 3:
-                    echo "thirty";
-                    break;
-                case 4:
-                    echo "forty";
-                    break;
-                case 5:
-                    echo "fifty";
-                    break;
-                case 6:
-                    echo "sixty";
-                    break;
-                case 7:
-                    echo "seventy";
-                    break;
-                case 8:
-                    echo "eighty";
-                    break;
-                case 9:
-                    echo "ninety";
-                    break;
-                case 10:
-                    echo "hundred";
-                    break;
+        // TH số nhập vào là số có 3 chữ số
+        if ($tram >= 1 && $tram < 10 && $chuc < 10 && $don_vi < 10) {
+            // TH sô chẵn trăm
+            if ($chuc == 0 && $don_vi == 0) {
+                for ($i = 0; $i < count($mangDon_vi); $i++) {
+                    switch ($tram) {
+                        case $i:
+                            echo "$so" . "=>" . "$mangDon_vi[$i]" . ' hundred';
+                            break;
+                    }
+                }
+            } else {
+                // TH số nhập vào có dạng x0x, x0y...
+                if ($chuc == 0) {
+                    for ($i = 0; $i < count($mangDon_vi); $i++) {
+                        for ($y = 0; $y < count($mangDon_vi); $y++) {
+                            switch ($tram) {
+                                case $i:
+                                    switch ($don_vi) {
+                                        case $y:
+                                            echo "$so" . "=>" . "$mangDon_vi[$i]" . ' hundred and ' . "$mangDon_vi[$y]";
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                } else {
+                    // TH số nhập vào có dạng x1x, x1y...
+                    if ($chuc == 1) {
+                        for ($i = 0; $i < count($mangDon_vi); $i++) {
+                            for ($y = 0; $y < count($mang10_19); $y++) {
+                                switch ($tram) {
+                                    case $i:
+                                        switch ($don_vi) {
+                                            case $y:
+                                                echo "$so" . "=>" . "$mangDon_vi[$i]" . ' hundred and ' . "$mang10_19[$y]";
+                                                break;
+                                        }
+                                        break;
+                                }
+                            }
+                        }
+                    } else {
+                        // TH số nhập vào có dạng xx0, xy0...
+                        if ($don_vi == 0) {
+                            for ($i = 0; $i < count($mangDon_vi); $i++) {
+                                for ($y = 0; $y < count($mangChuc); $y++) {
+                                    switch ($tram) {
+                                        case $i:
+                                            switch ($chuc) {
+                                                case $y:
+                                                    echo "$so" . "=>" . "$mangDon_vi[$i]" . ' hundred and ' . "$mangChuc[$y]";
+                                                    break;
+                                            }
+                                            break;
+                                    }
+                                }
+                            }
+                            // các TH còn lại
+                        } else {
+                            for ($i = 0; $i < count($mangDon_vi); $i++) {
+                                for ($u = 0; $u < count($mangChuc); $u++) {
+                                    for ($y = 0; $y < count($mangDon_vi); $y++) {
+                                        switch ($tram) {
+                                            case $i:
+                                                switch ($chuc) {
+                                                    case $u:
+                                                        switch ($don_vi) {
+                                                            case $y:
+                                                                echo "$so" . "=>" . "$mangDon_vi[$i]" . ' hundred and ' . "$mangChuc[$u]" . ' - ' . "$mangDon_vi[$y]";
+                                                                break;
+
+                                                        }
+                                                        break;
+                                                }
+                                                break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+    } else {
+        echo 'Out of ability';
     }
+}
 ?>
 </body>
 </html>
